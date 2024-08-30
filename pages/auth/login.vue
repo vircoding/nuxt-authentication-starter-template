dee75b8763cd94d786453afba4c4967c6511c86a
vircoding <nluismi80@gmail.com>
2024-08-30 06:23:51 -0400

    login form

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,7 +1,41 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+const input = ref({
+  email: '',
+  password: '',
+})
+
+async function onSubmit() {
+  try {
+    const data = await $fetch('/api/auth/login', {
+      method: 'POST',
+      body: input.value,
+    })
+
+    console.warn(data)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+</script>
 
 <template>
   <div>
     <h1>Login</h1>
+
+    <form @submit.prevent="onSubmit">
+      <input v-model="input.email" type="email" placeholder="Email">
+      <input v-model="input.password" type="password" placeholder="Password">
+      <button>Login</button>
+    </form>
   </div>
 </template>
+
+<style scoped>
+form {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
