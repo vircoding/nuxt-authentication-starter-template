1ae5f0087d80b3577ced55ddbadd0d38db2dca3c
vircoding <nluismi80@gmail.com>
2024-08-30 06:23:42 -0400

    register form

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,7 +1,45 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+const input = ref({
+  username: '',
+  email: '',
+  password: '',
+  repassword: '',
+})
+
+async function onSubmit() {
+  try {
+    const data = await $fetch('/api/auth/register', {
+      method: 'POST',
+      body: input.value,
+    })
+
+    console.warn(data)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+</script>
 
 <template>
   <div>
     <h1>Register</h1>
+
+    <form @submit.prevent="onSubmit">
+      <input v-model="input.username" type="text" placeholder="Username">
+      <input v-model="input.email" type="email" placeholder="Email">
+      <input v-model="input.password" type="password" placeholder="Password">
+      <input v-model="input.repassword" type="password" placeholder="Repassword">
+      <button>Login</button>
+    </form>
   </div>
 </template>
+
+<style scoped>
+form {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
