5626a3df9f84814168209a5e237c6b95eb408cb8
vircoding <nluismi80@gmail.com>
2024-08-30 23:18:21 -0400

    minor changes

--- composables/states.ts
+++ composables/states.ts
@@ -1,5 +1,5 @@
 // Access Token State
-export const useAccessToken = () => useState('accessToken')
+export const useAccessToken = () => useState<string>('accessToken')
 
 // User State
 export const useUser = () => useState<{ id: string, username: string, verified: boolean }>('user')
