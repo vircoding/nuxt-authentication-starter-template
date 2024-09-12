0265de3200d81ad3887562a86f25f4929d6bb6e8
vircoding <nluismi80@gmail.com>
2024-09-12 19:07:33 -0400

    minor changes

--- server/api/auth/test.post.ts
+++ server/api/auth/test.post.ts
@@ -17,64 +17,15 @@ export default defineEventHandler(async (event) => {
     // Validate the input
     const data = await loginSchema.parseAsync(input)
 
-    // Get the user-agent
-    const ua = getHeader(event, 'user-agent')
-
-    // Login the user
-    const { user, session } = await loginUser(data, ua)
-
-    // Generate the refresh token
-    const refreshToken = generateRefreshToken({ code: session.code, id: session.id, userId: session.userId })
-
-    // Generate the access token
-    const accessToken = generateAccessToken({ id: session.userId })
-
-    // Send the refresh token
-    setCookie(event, 'refresh_token', refreshToken, {
-      httpOnly: true,
-      sameSite: true,
-    })
-
-    return {
-      access_token: accessToken,
-      user: userTransformer(user),
-    }
+    return { data }
   }
   catch (error) {
-    console.error(error)
-
     // Body Error
     if (error instanceof BodyError) {
       throw createError({
         status: 400,
         statusMessage: 'Bad Request',
         message: 'Invalid JSON body',
-      })
-    }
-
-    // Zod Error handler
-    if (error instanceof ZodError) {
-      const fields = error.errors.map(issue => ({
-        field: issue.path.join('.'),
-        message: issue.message,
-      }))
-
-      throw createError({
-        status: 400,
-        statusMessage: 'Bad Request',
-        message: 'Invalid or missing required parameters',
-        data: {
-          fields,
-        },
-      })
-    }
-
-    // Bad Credentials Error handler
-    if (error instanceof CredentialsError) {
-      throw createError({
-        status: 401,
-        statusMessage: 'Unauthorized',
-        message: 'Bad Credentials',
       })
     }
 
